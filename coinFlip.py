b9d4185785dd71c02402a95373237cf0e6a28a78
Alexandr <12345678c40@gmail.com>
2022-06-19 14:37:59 +0300

    Finale

--- coinFlip.py
+++ coinFlip.py
@@ -1,11 +1,9 @@
 import random
 import time
-i = 0
-heads = 0
 print('Hello, im going to flip a coin for as times as u wish! than u try to guess!')
 time.sleep(2)
 name = input('Eneter your name first: ')
-def Asking():
+def Asking(i = 0, heads = 0):
 	sideOfCoin = input('Enter side u wonna track (heads or tails h/t): ')
 	if sideOfCoin == 'h':
 		sideOfCoin = 'heads'
@@ -17,6 +15,7 @@ def Asking():
 	print('doing calculations.../')
 	time.sleep(2)
 	while i < numOfFlips:
+		i = i + 1
 		if random.randint(0,1) == 1:
 			heads = heads + 1
 	if sideOfCoin == 'heads':
@@ -32,13 +31,10 @@ def Asking():
 				print('you almost got it! try higher next time! The result is:' + str(numOfFlips-heads))
 		else:
 				print('you almost got it! try lower next time! The result is:' + str(numOfFlips-heads))
-        i = i + 1
 
-        
 plyaAgain = 'yes'
 while plyaAgain != 'n':
 	Asking()
-	plyaAgain = input('do you want to play again? Type "n" if u want to exit')
+	plyaAgain = input('do you want to play again? Type "n" if u want to exit, press Enter to continue')
 
 input('Press Enter to leave')
-
