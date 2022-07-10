0ff9c24e507a4e2295e776f019c19afd33d2582b
Alexandr <12345678c40@gmail.com>
2022-07-10 16:38:24 +0300

    parser with a question

--- parcing/parser.py
+++ parcing/parser.py
@@ -1,10 +1,12 @@
 import requests
-
+import csv
 from bs4 import BeautifulSoup 
 
 URL = 'https://auto.ria.com/uk/legkovie/lexus/?page=1'
 HEADERS = {'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
  'accept':'*/*'}
+FILE = 'cars.csv'
+
 
 def getHtml(url, params=None):
     r = requests.get(url, headers=HEADERS, params=params)
@@ -24,14 +26,33 @@ def getContent(html):
             'link':item.find('a', class_='address').get('href'),
             'usdt_price':item.find('span', class_='green').get_text(strip=True),
             'mileage':item.find('li', class_='item-char js-race').get_text(strip=True),
-            #'engine':item.find('i', class_='icon-fuel').find_next_siblings(text=True),
+            # 'engine':item.find('i', class_='icon-fuel').find_next_siblings(text=True),
             'uah_price':uah_price.replace('\xa0грн','')
         })
-    print(cars)    
+    return cars    
+
+def getPagesNum(html): # returns 216 when there is 108 pages
+    soup = BeautifulSoup(html,'html.parser')
+    pagination = soup.find_all('span', class_='mhide')
+    return int(pagination[-1].get_text())
+
+def save_file(items, path):
+    with open(path, 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow(['Mark','Link','Dollar','Milage','UAH'])
+        for item in items:
+            writer.writerow([item['title'],item['link'],item['usdt_price'],item['mileage'],item['uah_price']])
 def parse():
     html = getHtml(URL)
     if html.status_code == 200:
-        getContent(html.text)
+        cars = []
+        pages_count = getPagesNum(html.text)
+        for page1 in range(1, pages_count + 1):
+            print(f'Process going on {page1} from {pages_count}...')
+            html = getHtml(URL, params = {'page' : page1})
+            cars.extend(getContent(html.text))
+        save_file(cars, FILE)
+        print(f'We got: {len(cars)} cars')
     else:
         print('Error, try again later')
 parse()
